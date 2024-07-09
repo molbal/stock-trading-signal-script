08f1489e5ce8701735348cdb1982eec728a0706a
molbal <molbal@outlook.com>
2024-07-09 22:08:50 +0200

    Adds debug reports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { getStocks, getStockBars } = require('./api');
 const { calculateIndicators } = require('./indicators');
-const { generateExcelReport } = require('./report');
+const { generateExcelReport, saveDebugReport } = require('./report');
 const tdqm = require('tqdm');
 const path = require('path');
 const fs = require('fs').promises;
@@ -9,6 +9,7 @@ async function main() {
   await fs.mkdir('charts', { recursive: true });
   const stocks = await getStocks();
   const results = [];
+  const debug = [];
   console.log('Listed', stocks.length, 'stocks');
 
   for (const stock of tdqm(stocks)) {
@@ -19,11 +20,16 @@ async function main() {
     const { ema200, macd } = calculateIndicators(bars);
     const currentPrice = bars[bars.length - 1].c;
 
+    const isCurrentPriceAboveEMA200 = currentPrice > 1.03 * ema200[ema200.length - 1];
+    const isMACDLessThanSignalDayA = macd[macd.length - 2].MACD < macd[macd.length - 2].signal;
+    const isMACDGreaterThanSignalDayB = macd[macd.length - 1].MACD > macd[macd.length - 1].signal;
+    const isMACDAndSignalNegative = macd[macd.length - 1].MACD < 0 && macd[macd.length - 1].signal < 0;
+
     if (
-      currentPrice > ema200[ema200.length - 1] &&
-      macd[macd.length - 1].MACD > macd[macd.length - 1].signal &&
-      macd[macd.length - 1].signal < 0 &&
-      currentPrice > 1.03 * ema200[ema200.length - 1]
+      isCurrentPriceAboveEMA200 &&
+      isMACDLessThanSignalDayA &&
+      isMACDGreaterThanSignalDayB &&
+      isMACDAndSignalNegative
     ) {
       const result = {
         symbol: stock.symbol,
@@ -34,12 +40,23 @@ async function main() {
       };
       results.push(result);
       console.log('💵💵💵 Signal!', result);
-
     }
+    debug.push({
+      isCurrentPriceAboveEMA200: isCurrentPriceAboveEMA200 ? 'true' : 'false',
+      isMACDLessThanSignalDayA: isMACDLessThanSignalDayA ? 'true' : 'false',
+      isMACDGreaterThanSignalDayB: isMACDGreaterThanSignalDayB ? 'true' : 'false',
+      isMACDAndSignalNegative: isMACDAndSignalNegative ? 'true' : 'false',
+      symbol: stock.symbol,
+      name: stock.name,
+      currentPrice: currentPrice,
+      ema200: ema200[ema200.length - 1],
+      macd: macd[macd.length - 1],
+    });
   }
 
   console.log('Exporting', results.length, 'results to xlsx.');
   await generateExcelReport(results);
+  await saveDebugReport(debug);
   console.log('Done. Good luck trading.');
 }
 
